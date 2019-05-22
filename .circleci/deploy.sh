84c697e709363137c8d7222816f7d293eccbf55a
sheeta_mm <atan.sheeta@icloud.com>
2019-05-22 14:00:01 +0900

    Update deploy.sh

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -6,7 +6,7 @@ set -e
 DEPLOY_DIR=deploy
 
 # gitの諸々の設定
-git config --global push.default simple
+git config --global push.default master
 git config --global user.email $(git --no-pager show -s --format='%ae' HEAD)
 git config --global user.name $CIRCLE_USERNAME
 
@@ -15,7 +15,7 @@ git clone -q --branch=master $CIRCLE_REPOSITORY_URL $DEPLOY_DIR
 
 # rsyncでhugoで生成したHTMLをコピー
 cd $DEPLOY_DIR
-rsync -arv --delete ../docs/* .
+rsync -arv --delete ../docs/* ./docs/
 
 git add -f .
 git commit -m "Deploy #$CIRCLE_BUILD_NUM from CircleCI [ci skip]" || true
